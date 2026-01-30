31427fdd71ba439b8efcd707d327a712dfb15462
Madeep Yampalaku <111452217+Madeep9347@users.noreply.github.com>
2026-01-30 18:36:54 +0530

    Update GraphQL endpoint and API key

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import App from "./App";
 Amplify.configure({
   API: {
     GraphQL: {
-      endpoint: "https://yxfxvoax2rbtpbsp5wpp7wraxm.appsync-api.ap-south-1.amazonaws.com/graphql",
+      endpoint: "https://n3yil3f55zfzdd4jmmad6trm3y.appsync-api.ap-south-1.amazonaws.com/graphql",
       region: "ap-south-1",
       defaultAuthMode: "apiKey",
-      apiKey: "da2-7vxacrllebfibnqaakcvkyj6ny"
+      apiKey: "da2-6vmuticfhnff5nmhsmvwhld2nm"
     }
   }
 });
